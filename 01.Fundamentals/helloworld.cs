66b4d46a222149ee80107f598f307486764b93f7
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2024-12-06 11:47:27 +0600

    first commit

---
+++ 01.Fundamentals/helloworld.cs
@@ -0,0 +1,25 @@
+// C# program to print Hello World! 
+using System;
+
+// namespace declaration 
+namespace HelloWorldApp
+{
+
+    // Class declaration 
+    class Learn
+    {
+
+        // Main Method 
+        static void Main(string[] args)
+        {
+
+            // statement 
+            // printing Hello World! 
+            Console.WriteLine("Hello World starting new class  !");
+
+            // To prevents the screen from 
+            // running and closing quickly 
+            Console.ReadKey();
+        }
+    }
+}
