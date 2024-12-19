221c2c5747445aa3ad97a3b9bc8fd9a02eb6cb27
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2024-12-19 22:21:58 +0600

    veriables

---
+++ 01.Fundamentals/veriables.cs
@@ -0,0 +1,24 @@
+
+using System;
+class Geeks
+{
+
+
+    int y;
+
+
+    public static void Main(String[] args)
+    {
+
+
+        int x = 32;
+
+
+        Console.WriteLine("Value of x is " + x);
+
+
+        Geeks gfg = new Geeks();
+ 
+        Console.WriteLine("Value of y is " + gfg.y);
+    }
+}
