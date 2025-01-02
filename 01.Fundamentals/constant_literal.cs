dd55c1f636c47a082898c40b142e125e1c48708f
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2025-01-02 20:48:47 +0600

    const&literal

---
+++ 01.Fundamentals/constant_literal.cs
@@ -0,0 +1,23 @@
+// C# program to illustrate the use of Integer Literals
+using System;
+
+class Geeks{
+
+	// Main method
+	public static void Main(String[] args)
+	{ 
+	
+		// decimal-form literal
+		int a = 101;
+
+		// Hexa-decimal form literal
+		int c = 0xFace;
+	
+		// binary-form literal
+		int x = 0b101;
+		
+		Console.WriteLine(a);
+		Console.WriteLine(c);
+		Console.WriteLine(x);
+	}
+}
